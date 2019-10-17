be39ac7a173a31f348cc3c95833998384bec822e
leochan007 <leochan007@163.com>
2019-10-17 17:38:44 +0800

    add waitgroup for main.

--- go_src/main.go
+++ go_src/main.go
@@ -3,76 +3,101 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
 
+	"sync"
 	"github.com/urfave/cli"
 	"github.com/op/go-logging"
 	. "github.com/leochan007/xtp.go/go_src/xtp_wrapper"
 )
 
-type argT struct {
-	QuoteHost  string `cli:"q,quotehost" usage:"quotehost" prompt:"type quotehost"`
-	QuotePort  int    `cli:"p,quoteport" usage:"quoteport" prompt:"type quoteport"`
-	TraderHost string `cli:"r,traderhost" usage:"traderhost" prompt:"type traderhost"`
-	TraderPort int    `cli:"o,traderport" usage:"traderport" prompt:"type traderport"`
-	Username   string `cli:"u,username" usage:"account" prompt:"type account"`
-	Password   string `pw:"w,password" usage:"password of account" prompt:"type the password"`
-	Debug      bool   `cli:"d,debug" usage:"debug flag" prompt:"if it is in debug mode"`
-	File       string `cli:"f,file" usage:"file for strategy" prompt:"type json file name"`
-}
-
 type StockMap map[string]string
 
 var (
-	log = logging.MustGetLogger("main.go")
+	log = logging.MustGetLogger("go_xtp_trader")
 	Stocks = make(StockMap)
+	wg sync.WaitGroup
+	c chan os.Signal
 )
 
-func main() {
-	app := cli.NewApp()
-	app.UseShortOptionHandling = true
-	app.Commands = []cli.Command{
-		{
-			Name:  "short",
-			Usage: "complete a task on the list",
-			Flags: []cli.Flag{
-				cli.StringFlag{Name: "quotehost, q"},
-				cli.StringFlag{Name: "quoteport, p"},
-				cli.StringFlag{Name: "traderhost, r"},
-				cli.StringFlag{Name: "traderport, o"},
-				cli.StringFlag{Name: "username, u"},
-				cli.StringFlag{Name: "password, w"},
-				cli.StringFlag{Name: "file, f"},
-				cli.BoolFlag{Name: "debug, d"},
-			},
-			Action: func(c *cli.Context) error {
-				fmt.Println("quotehost:", c.String("quotehost"))
-				fmt.Println("quoteport:", c.String("quoteport"))
-				fmt.Println("traderhost:", c.String("traderhost"))
-				fmt.Println("traderport:", c.Int("traderport"))
-				fmt.Println("username:", c.String("username"))
-				fmt.Println("password:", c.String("password"))
-				fmt.Println("file:", c.String("file"))
-				fmt.Println("debug:", c.Bool("debug"))
-
-				folder := "bin"
-
-				trader_api := GoCreateLCTraderApi(1, folder)
-				trader_spi := GoCreateLCTraderSpi()
-				Go_trader_apiRegisterSpi(trader_api, trader_spi)
-				loginResult := Go_trader_apiLogin(trader_api, c.String("traderhost"), c.Int("traderport"), c.String("username"), c.String("password"))
-
-				if loginResult == 0 {
-					fmt.Println("trader login OK.")
-				} else {
-					fmt.Println("trader failed.")
-				}
-
-				return nil
-			},
-		},
+func waitForSignal() {
+	LOOP:
+	for {
+		select {
+			case <-c:
+			break LOOP
+		default:
+		}
 	}
 
+	log.Info("---waitForSignal---")
+	wg.Done()
+}
+
+func main() {
+	
+	c = make(chan os.Signal, 1)
+    signal.Notify(c, os.Interrupt, os.Kill)
+
+	var quotehost string
+	var quoteport int
+	var traderhost string
+	var traderport int
+	var username string
+	var password string
+	var host string
+	var file string
+	var debug bool
+
+	app := cli.NewApp()
+	app.UseShortOptionHandling = true
+	app.Version = "0.0.1"
+	app.Flags = []cli.Flag {
+		cli.StringFlag{Name: "quotehost, q", Destination: &quotehost},
+		cli.IntFlag{Name: "quoteport, p", Destination: &quoteport},
+		cli.StringFlag{Name: "traderhost, r", Destination: &traderhost},
+		cli.IntFlag{Name: "traderport, o", Destination: &traderport},
+		cli.StringFlag{Name: "username, u", Destination: &username},
+		cli.StringFlag{Name: "password, w", Destination: &password},
+		cli.StringFlag{Name: "host, H", Destination: &host},
+		cli.StringFlag{Name: "file, f", Destination: &file},
+		cli.BoolFlag{Name: "debug, d", Destination: &debug},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("Hello %q", c.Args())
+
+		fmt.Println("quotehost:", quotehost)
+		fmt.Println("quoteport:", quoteport)
+		fmt.Println("traderhost:", traderhost)
+		fmt.Println("traderport:", traderport)
+		fmt.Println("username:", username)
+		fmt.Println("password:", password)
+		fmt.Println("file:", file)
+		fmt.Println("debug:", debug)
+
+		folder := "xtp_con"
+
+		trader_api := GoCreateLCTraderApi(1, folder)
+		trader_spi := GoCreateLCTraderSpi()
+		Go_trader_apiRegisterSpi(trader_api, trader_spi)
+		loginResult := Go_trader_apiLogin(trader_api, traderhost, traderport, username, password)
+
+		if loginResult == 0 {
+			fmt.Println("trader login OK.")
+		} else {
+			fmt.Println("trader failed.")
+		}
+
+		wg.Add(1)
+		go waitForSignal()
+		wg.Wait()
+
+		return nil
+	  }
+
 	err := app.Run(os.Args)
+
 	if err != nil {
 		log.Fatal(err)
 	}
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cur_dir=`pwd`
-CGO_LDFLAGS="-L$cur_dir -lCTPCApi -lthostmduserapi -lthosttraderapi" LD_LIBRARY_PATH="$cur_dir" ./go_ctp_trader -f=strategy_d.json -H=0.0.0.0:18080
+CGO_LDFLAGS="-L$cur_dir -lCXTPApi -lxtpquoteapi -lxtptraderapi" LD_LIBRARY_PATH="$cur_dir" \
+    ./go_xtp_trader --file strategy_d.json --host 0.0.0.0:18080
+    
